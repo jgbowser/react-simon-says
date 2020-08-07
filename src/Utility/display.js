2b6e23ca6036f3bb53c1ca37fcfb528c7da74c9e
John Bowser <john.bow7720@gmail.com>
2020-08-07 14:35:57 -0600

    blank utility files added

---
+++ src/Utility/display.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+// build out functions for game display here
+//light up computer selections
+//light up player selections
+//rotating lights on start screen
---
+++ src/Utility/gameplay.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// Need: overall game loop function; contains:
+// Computer turn function: pushes random number to computer array
+// Player turn function: compares a fresh array for each turn to the computer array on each action
+// manage GAME_OVER in state. loop game while GAME_OVER === false
+// when player makes a mistake setState GAME_OVER to true.
+
+// once gameplay functionality is built out work on display (light up buttons, etc) functionality. Probably in a different file
